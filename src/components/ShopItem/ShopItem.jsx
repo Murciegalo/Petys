bf0668408f0db4feed3dda8aa21855f65605797a
olo <snash@tutanota.com>
2022-02-23 10:19:02 -0300

    shop layout in progress

---
+++ src/components/ShopItem/ShopItem.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Cont } from './ShopItem.styles'
+
+const ShopItem = () => {
+  return <Cont>
+    Shop Item
+  </Cont>
+}
+
+export default ShopItem
--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import ShopItem from '../../components/ShopItem/ShopItem'
+import { Pomeranian } from '../../data'
 import { 
-  Cont, Item, ItemsList, WrapperF, WrapperS 
+  Cont, ItemsList, WrapperF, WrapperS 
 } from './Shop.styles'
 
 const Shop = () => {
@@ -11,7 +13,9 @@ const Shop = () => {
       </WrapperF>
       <WrapperS>
         <ItemsList>
-          <Item />
+          {
+            Pomeranian.map(el => <ShopItem key={el.id} el={el} /> )
+          }
         </ItemsList>
       </WrapperS>
     </Cont>
