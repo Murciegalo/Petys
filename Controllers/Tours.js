d71d9c4c170d30ed0ca8773e15b2db6f10da474d
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2022-03-07 06:51:43 -0300

    Middleware set for param Id

--- Controllers/Tours.js
+++ Controllers/Tours.js
@@ -1,4 +1,14 @@
-
+// PARAM MIDDLEWARE
+exports.checkID = (req, res, next, val) => {
+  console.log(`Tour Id: ${val}`);
+  // if(req.params.id * 1 > tours.length){
+  //   return res.status(404).json({
+  //     status: 'failed',
+  //     msg: 'Invalid ID'
+  //   })
+  // }
+  next()
+}
 
 exports.getAllTours = (req, res, next) => {
   return res.status(200).json({ 
@@ -7,5 +17,7 @@ exports.getAllTours = (req, res, next) => {
 }
 
 exports.getTour = (req, res, next) => {
-  return res.status(200).json({ msg: 'all ok' })
+  return res.status(200).json({ 
+    msg: 'all ok' 
+  })
 }
--- Routes/Tours.js
+++ Routes/Tours.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const {
   getAllTours,
-  getTour
+  getTour,
+  checkID
 } = require('../Controllers/Tours');
 
-
 const router = express.Router();
+
+// MIDDLEWARES
+router.param('id', checkID)
 
 
 // TOURS
 router.get( '/', getAllTours );
 router.get( '/:id', getTour );
+
+
 
 module.exports = router;
