5636d0eb54ff6253ec796a6f0e0674bd0be99496
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2022-03-07 08:46:14 -0300

    DB connected to App

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-const dotenv = require('dotenv')
-dotenv.config({path: './config.env'})
-
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
 const app = require('./app');
 
+mongoose.connect(process.env.DB, {}).then((el) => {
+  // console.log(el.connections);
+  console.log('DB connected');
+});
 
-app.listen( process.env.PORT, () => {
+app.listen(process.env.PORT, () => {
   console.log(`App running in port:${process.env.PORT}`);
-})
+});
