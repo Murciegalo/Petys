e341071b42b1f26be2e3a6beb85d603b5c1ee8d9
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2022-03-08 07:37:46 -0300

    Model for Pets in progress

--- Models/PetModel.js
+++ Models/PetModel.js
@@ -4,15 +4,52 @@ const petSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'A pet needs a name'],
+    trim: true,
   },
-  pedigree: {
+  pedigreeM: {
     type: Number,
     required: [true, 'A pet needs a pedigree'],
     unique: true,
   },
+  pedigreeF: {
+    type: Number,
+    required: [true, 'A pet needs a pedigree'],
+    unique: true,
+  },
+  description: {
+    type: String,
+    required: [true, 'A pet needs a description'],
+    trim: true,
+  },
   price: {
     type: Number,
     required: [true, 'A pet needs a price'],
+  },
+  priceDiscount: Number,
+  imgCover: {
+    type: String,
+    required: [true, 'A pet needs an image cover'],
+  },
+  imgs: [
+    {
+      type: String,
+    },
+  ],
+  supplier: {
+    type: String,
+    required: [true, 'A pet needs a seller'],
+  },
+  ratingsAvrgSupplier: {
+    type: Number,
+    default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
   },
 });
 
---
+++ data.js
@@ -0,0 +1,53 @@
+const pets = [
+  {
+    name: 'Lulu de Pomerania',
+    pedigreeM: '',
+    pedigreeF: '',
+    description: '',
+    price: 1.0,
+    priceDiscount: 0,
+    imgCover: '',
+    imgs: [
+      {
+        src: '',
+      },
+    ],
+    supplier: 'Criadero Fernandez',
+    ratingsAvrgSupplier: '',
+    ratingsQuantity: 20,
+  },
+  {
+    name: 'Lulu de Pomerania',
+    pedigreeM: '',
+    pedigreeF: '',
+    description: '',
+    price: 1.0,
+    priceDiscount: 0,
+    imgCover: '',
+    imgs: [
+      {
+        src: '',
+      },
+    ],
+    supplier: 'Criadero Fernandez',
+    ratingsAvrgSupplier: '',
+    ratingsQuantity: 20,
+  },
+  {
+    name: 'Lulu de Pomerania',
+    pedigreeM: '',
+    pedigreeF: '',
+    description: '',
+    price: 1.0,
+    priceDiscount: 0,
+    imgCover: '',
+    imgs: [
+      {
+        src: '',
+      },
+    ],
+    supplier: 'Criadero Fernandez',
+    ratingsAvrgSupplier: '',
+    ratingsQuantity: 20,
+  },
+];
