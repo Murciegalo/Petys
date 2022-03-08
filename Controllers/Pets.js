fd038c53ffe1dbaf87f138ddbbdb389a821a7960
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2022-03-08 18:44:32 -0300

    Sort filter implemented

--- Controllers/Pets.js
+++ Controllers/Pets.js
@@ -11,6 +11,15 @@ exports.getAllPets = async (req, res) => {
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
     // console.log(JSON.parse(queryStr));
+    // return query from f()
+
+    //SORTING
+    // if (req.query.sort) {
+    //   const sortBy = req.query.sort.split(',').join(' ');
+    //   query = query.sort(sortBy)
+    // } else{
+    //  query = query.sort('-createdAt')
+    // }
 
     // EXECUTE QUERY
     const data = await Pet.find(JSON.parse(queryStr));
