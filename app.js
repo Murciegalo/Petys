2e6d8ac318908deee99c06a7f7160fad1807b415
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2022-03-16 16:53:43 -0300

    http pollution parameters security measures set

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
-// reqs/ip on a given time
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 //ROUTERS
 const petRouter = require('./Routes/pets');
 const userRouter = require('./Routes/user');
@@ -9,17 +12,32 @@ const userRouter = require('./Routes/user');
 const app = express();
 
 //GLOBAL MIDDLEWARES
+// Security HTTP headers
+app.use(helmet());
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+// Limit reqs/ip on a given time
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
   message: 'Plase come back later',
 });
 app.use('/api', limiter);
+// req.body
+app.use(express.json({ limit: '10kb' }));
 
-app.use(express.json());
+// Data sanitization vs NOSQL query injections
+app.use(mongoSanitize());
+// Data sanitization vs XSS
+app.use(xss());
+// Prevent parameters pollution
+app.use(
+  hpp({
+    whitelist: ['price', 'supplier'],
+  })
+);
+
 app.use('/api/v1/pets', petRouter);
 app.use('/api/v1/user', userRouter);
 
