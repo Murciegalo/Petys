c2bac8ef6a3f04ba6afe64ff4618d2d9544d442d
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2022-03-17 17:30:58 -0300

    ReviewModel in pgress

--- Models/ReviewModel.js
+++ Models/ReviewModel.js
@@ -1,6 +1,35 @@
 const mongoose = require('mongoose');
 
-const reviewSchema = new mongoose.Schema({});
+const reviewSchema = new mongoose.Schema({
+  review: {
+    type: String,
+    required: [true, 'Please fill in this field'],
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    select: false,
+  },
+  rating: {
+    type: Number,
+    required: [true, 'Please fill in this field'],
+  },
+  petReviewed: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Pet',
+  },
+  userReview: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+});
 
 const Review = mongoose.model('Review', reviewSchema);
 
