7c651a25763db9d1564263e4b1f85c9f2e864bfc
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2022-03-29 10:21:57 -0300

    reselect set

--- Client/src/store/user/user.selector.js
+++ Client/src/store/user/user.selector.js
@@ -4,8 +4,3 @@ import { createSelector } from 'reselect';
 const getUser = (state) => state.user;
 
 export const grabUser = createSelector([getUser], (all) => all.user);
-export const grabUserSignUp = createSelector([getUser], (all) => all.user);
-export const grabLoading = createSelector([getUser], (all) => all.loading);
-export const grabError = createSelector([getUser], (all) => all.error);
-export const grabAuth = createSelector([getUser], (all) => all.isAuth);
-export const grabProfile = createSelector([getUser], (all) => all.profile);
