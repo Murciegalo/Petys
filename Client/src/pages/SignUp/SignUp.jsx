f2b2a08d4dc8cf8136095318cafec547c6d412df
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2022-03-30 16:36:45 -0300

    Redux SignUp in progress

--- Client/src/pages/SignUp/SignUp.jsx
+++ Client/src/pages/SignUp/SignUp.jsx
@@ -1,22 +1,75 @@
-import React from 'react';
+import { useState } from 'react';
+import { connect } from 'react-redux';
+import { register } from '../../store/user/user.actions';
+// import { Navigate } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import { Cont, Header, Wrapper, Form, Input, Text } from '../../components/SignIn/Sign.styles';
 import { Button, TypeBtn } from '../../components/Button/Button';
 
-const SignUp = () => {
+const SignUp = ({ isAuth, loading, register }) => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPass, setConfirmPass] = useState('');
+
+  const handleSubmit = (e) => {
+    if (password === confirmPass) {
+      return;
+    }
+    register(name, email, password);
+    e.preventDefault();
+    console.log('submit');
+  };
   return (
     <Cont>
       <Header>Sign Up</Header>
       <Wrapper>
-        <Form>
-          <Input placeholder="Full Name" />
-          <Input placeholder="Email" />
-          <Input placeholder="Password" />
-          <Button btnType={TypeBtn.signInUp}>Sign Up</Button>
+        <Form onSubmit={handleSubmit}>
+          <Input
+            onChange={(e) => setName(e.target.value)}
+            type="name"
+            placeholder="Full Name"
+            value={name}
+            required
+          />
+          <Input
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            placeholder="Email"
+            value={email}
+            required
+          />
+          <Input
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            placeholder="Password"
+            value={password}
+            required
+          />
+          <Input
+            onChange={(e) => setConfirmPass(e.target.value)}
+            type="password"
+            placeholder="Confirm password"
+            value={confirmPass}
+            required
+          />
+          <Button type="submit" btnType={TypeBtn.signInUp}>
+            Sign Up
+          </Button>
           <Text to="/login">Already have an account? Login</Text>
         </Form>
       </Wrapper>
     </Cont>
   );
 };
+SignUp.propTypes = {
+  register: PropTypes.func.isRequired,
+  isAuth: PropTypes.bool,
+  loading: PropTypes.bool,
+};
+const mapStateToProps = (state) => ({
+  isAuth: state.user.isAuth,
+  loading: state.user.loading,
+});
 
-export default SignUp;
+export default connect(mapStateToProps, { register })(SignUp);
