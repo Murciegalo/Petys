38d25e04c3bc7182e63ec12a620fb3ee713800d8
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2022-04-05 10:29:47 -0300

    bug found at store.js & fixed

--- Client/src/store/store.js
+++ Client/src/store/store.js
@@ -1,11 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
-// import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import { rootReducer } from './root-reducer';
 import logger from 'redux-logger';
 
 const initialState = {};
-
 const middlewares = [thunk, logger];
 
 const store = createStore(rootReducer, initialState, applyMiddleware(...middlewares));
