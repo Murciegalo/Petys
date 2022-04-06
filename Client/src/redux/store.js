a9e99e9facdc399be4f2e38ae6195a37a4e50bff
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2022-04-06 08:22:57 -0300

    implementing redux-sagas in progress

--- Client/src/redux/store.js
+++ Client/src/redux/store.js
@@ -1,8 +1,10 @@
 import { compose, createStore, applyMiddleware } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
+import createSagaMiddleware from 'redux-saga';
 import logger from 'redux-logger';
 import { rootReducer } from './root-reducer';
+import { rootSaga } from './root-saga';
 
 const persistConfig = {
   key: 'root',
@@ -11,8 +13,15 @@ const persistConfig = {
 };
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middlewares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean);
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [process.env.NODE_ENV !== 'production' && logger, sagaMiddleware].filter(
+  Boolean,
+);
 const composeEnhancers = compose(applyMiddleware(...middlewares));
 
 export const store = createStore(persistedReducer, undefined, composeEnhancers);
+
+sagaMiddleware.run(rootSaga);
+
 export const persistor = persistStore(store);
