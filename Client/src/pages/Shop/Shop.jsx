e9938e5089fc3134d8663731714bb25dff012edc
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2022-04-12 10:08:54 -0300

    whole refactoring for shopPg & memoized-selectors to be init

--- Client/src/pages/Shop/Shop.jsx
+++ Client/src/pages/Shop/Shop.jsx
@@ -1,10 +1,9 @@
 import React, { useEffect } from 'react';
-import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 import ShopItem from '../../components/ShopItem/ShopItem';
 import { getAllPetsStart } from '../../redux/pets/pet.actions';
 import { Pomeranian } from '../../utils/data';
-import { Cont, WrapperF, Option, WrapperS } from './Shop.styles';
+import { Cont, WrapperS } from './Shop.styles';
 
 const Shop = () => {
   const dispatch = useDispatch();
@@ -13,15 +12,6 @@ const Shop = () => {
   }, []);
   return (
     <Cont>
-      <WrapperF>
-        <Option to="/shop/luludepomerania">Lulu de Pomerania</Option>
-        <Option to="/shop/luludepomerania">Shih Tzu</Option>
-        <Option to="/shop/luludepomerania">Poodle</Option>
-        <Option to="/shop/luludepomerania">Golden Retriever</Option>
-        <Option to="/shop/luludepomerania">Yorkshire</Option>
-        <Option to="/shop/luludepomerania">Husky Siberiano</Option>
-        <Option to="/shop/luludepomerania">Buldogue francês</Option>
-      </WrapperF>
       <WrapperS>
         {Pomeranian.map((el) => (
           <ShopItem key={el.id} el={el} />
