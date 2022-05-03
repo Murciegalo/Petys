b76b52b415c9190d1a926729e5548f88a2d0151f
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2022-05-03 10:07:33 -0300

    Building robust & complex emails_handler for App

--- utils/email.js
+++ utils/email.js
@@ -1,22 +1,75 @@
 const nodemailer = require('nodemailer');
+const { convert } = require('html-to-text');
 
-const sendEmail = async (options) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_ADDRESS,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
-  const mailOptions = {
-    from: 'Testing App <testing@gml.com>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-  };
+// new Email(user, url)
+module.exports = class Email {
+  constructor() {
+    this.to = user.email;
+    this.name = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Testing App <${process.env.EMAIL_CLASS_FROM}>`;
+    this.x = true;
+  }
 
-  await transporter.sendMail(mailOptions);
+  //Development
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      //Sendgrid to be set here
+      return 1;
+    }
+    return nodemailer.createTransport({
+      host: process.env.HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_ADDRESS,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+  }
+
+  //Send email
+  async send(subject) {
+    // 1) Render HTML
+    const htmlW = `<p>Hi ${this.firstName},</p>
+      <p>Welcome to Freaking Tours, we're glad to have you</p> 🎉🙏
+      <a href=${this.url} target='_blank'> Upload user photo</a>
+      <p>If you need any help, please don't hesitate to contact us!</p>
+    `;
+    const htmlF = `<p>Hi ${this.firstName}</p>
+      <p>Forgot your password? 
+      Submit a PATCH request with your new password and passwordConfirm to: ${this.url}.</p>
+      <a href=${this.url} target='_blank'> Reset your password</a>
+      <p> If you didn't request a new password , please ignore this email</p>
+    `;
+
+    // 2. Define email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html: this.x ? htmlW : htmlF,
+      text: convert(htmlW, {
+        selectors: [
+          // url to be updated
+          { selector: 'a', options: { baseUrl: 'https://example.com' } },
+          { selector: 'p', format: 'skip' },
+        ],
+      }),
+    };
+
+    // 3) Create a transport and send email
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    this.x = true;
+    await this.send('Welcome to the FreakingTours Family!');
+  }
+
+  async sendPasswordReset() {
+    this.x = false;
+    await this.send('Forgot your password mate? no worries');
+  }
 };
 
 module.exports = sendEmail;
